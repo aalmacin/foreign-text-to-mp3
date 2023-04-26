de340518ce6efa21898ceec9aff9234ab4189c6d
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2023-04-25 23:27:05 -0600

    Initial commit

---
+++ src/main/java/com/raidrin/foreigntexttomp3/ForeignTextToMp3Application.java
@@ -0,0 +1,13 @@
+package com.raidrin.foreigntexttomp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignTextToMp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeignTextToMp3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/raidrin/foreigntexttomp3/ForeignTextToMp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidrin.foreigntexttomp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignTextToMp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
