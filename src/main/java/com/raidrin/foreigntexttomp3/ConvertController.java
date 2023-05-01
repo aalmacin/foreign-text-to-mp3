218df7389537594fc522dba7fbec2cda154f25c6
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2023-04-30 21:57:06 -0600

    Add validation

--- src/main/java/com/raidrin/foreigntexttomp3/ConvertController.java
+++ src/main/java/com/raidrin/foreigntexttomp3/ConvertController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ConvertController {
     @GetMapping("/")
@@ -14,9 +17,28 @@ public class ConvertController {
     }
 
     @PostMapping("/generate")
-    public String generate(@RequestParam String text, @RequestParam String lang, Model model) {
+    public String generate(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) String lang,
+            @RequestParam(name = "multi-mp3", required = false) Boolean multipleMP3,
+            Model model
+    ) {
+        List<String> errors = new ArrayList<>();
+        boolean emptyText = text == null || text.isEmpty();
+        boolean emptyLang = lang == null || lang.isEmpty();
+        if (emptyText) {
+            errors.add("Text cannot be empty");
+        }
+        if(emptyLang) {
+            errors.add("Language cannot be empty");
+        }
+        if(emptyLang || emptyText) {
+            model.addAttribute("errors", errors);
+            return "index";
+        }
         model.addAttribute("text", text);
         model.addAttribute("lang", lang);
+        model.addAttribute("multiMP3", multipleMP3 ? "multi mp3" : "single mp3");
         return "generated";
     }
 
